bdb808fd2a475a9370fceaed117a484d073b4ce6
Kirill <seeraydary@gmail.com>
2022-05-31 07:39:04 +0300

    Сортировка нечетных чисел в массиве в порядке возрастания, игнорирование четных чисел

---
+++ src/sort_array/Kata.java
@@ -0,0 +1,43 @@
+package sort_array;
+
+// Сортировка нечетных чисел в массиве в порядке возрастания, игнорирование четных чисел
+
+import java.util.Arrays;
+
+public class Kata {
+
+    public static void main(String[] args) {
+        int[] kata = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+        sortArray(kata);
+    }
+
+    public static int[] sortArray(int[] array) {
+        int i;
+        int n;
+        int m;
+        int temp = 0;
+        if (array.length < 2) {
+            System.out.println(Arrays.toString(array));
+            return array;
+        }
+
+        for (i = 0; i < array.length; i++) {
+            for (n = 0; n < array.length; n++) {
+                if (array[n] % 2 != 0) {
+                    for (m = n + 1; m < array.length; m++) {
+                        if (array[m] % 2 != 0) {
+                            if (array[n] > array[m]) {
+                                temp = array[n];
+                                array[n] = array[m];
+                                array[m] = temp;
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(Arrays.toString(array));
+        return array;
+    }
+}
