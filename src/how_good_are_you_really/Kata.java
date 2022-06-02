21d9e68838d275baa9890c912b5f706c2b91de40
Kirill <seeraydary@gmail.com>
2022-06-02 18:59:22 +0300

    Возвращает true, если твоя оценка лучше, false - если нет

---
+++ src/how_good_are_you_really/Kata.java
@@ -0,0 +1,27 @@
+package how_good_are_you_really;
+
+// Возвращает true, если твоя оценка лучше, false - если нет
+
+public class Kata {
+
+    public static void main(String[] args) {
+        int[] gradesOfTheClass = {1, 4, 4, 3, 3, 2, 2, 2, 4};
+        System.out.println(betterThanAverage(gradesOfTheClass, 5));
+    }
+
+    public static boolean betterThanAverage(int[] classPoints, int yourPoints) {
+        int average = 0;
+        if (classPoints.length > 0) {
+            int sum = 0;
+            for (int i = 0; i < classPoints.length; i++) {
+                sum += classPoints[i];
+            }
+            average = sum / classPoints.length;
+        }
+        if (average >= yourPoints) {
+            return false;
+        }
+        return true;
+    }
+}
+
