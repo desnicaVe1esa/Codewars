1ec27a745c2f1e39dbd0064706b3bf1814762d99
Kirill <seeraydary@gmail.com>
2022-10-15 15:58:29 +0300

    Функцию, которая возвращает true, если одно число четное, а второе нечетное. Иначе false

---
+++ src/opposites_attract/OppositesAttract.java
@@ -0,0 +1,17 @@
+package opposites_attract;
+
+// Функцию, которая возвращает true, если одно число четное, а второе нечетное. Иначе false
+
+public class OppositesAttract {
+
+    public static boolean isLove(final int flower1, final int flower2) {
+        if (flower1 == 0 || flower2 == 0) {
+            return true;
+        }
+        if (flower1 % 2 != 0 && flower2 % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
