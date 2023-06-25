924204e42f02d671c5855563a009a68e4e3f2813
Kirill <seeraydary@gmail.com>
2023-06-25 14:44:51 +0300

    Функция, которая принимает строку из одного или нескольки слов и возвращает ту же строку, но со всеми пятью или более буквенными словами в обратном порядке.

--- src/stop_gninnips_my_sdrow/SpinWordsTest.java
+++ src/stop_gninnips_my_sdrow/SpinWordsTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertEquals;
 public class SpinWordsTest {
     @Test
     public void test() {
-//        assertEquals("emocleW", new SpinWords().spinWords("Welcome"));
+        assertEquals("emocleW", new SpinWords().spinWords("Welcome"));
         assertEquals("Hey wollef sroirraw", new SpinWords().spinWords("Hey fellow warriors"));
     }
 
