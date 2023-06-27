32dd09215802001757270b85849dbd9e4ddc07b3
Kirill <seeraydary@gmail.com>
2023-06-27 14:46:29 +0300

    Функция, которая форматирует длительность, заданную в виде количества секунд, удобным для человека способом.

--- src/human_readable_duration_format/TimeFormatter.java
+++ src/human_readable_duration_format/TimeFormatter.java
@@ -15,10 +15,10 @@ public class TimeFormatter {
                 Arrays.stream(
                                 new String[]{
                                         formatTime("year",  (seconds / 31536000)),
-                                        formatTime("day",   (seconds / 86400)%365),
-                                        formatTime("hour",  (seconds / 3600)%24),
-                                        formatTime("minute",(seconds / 60)%60),
-                                        formatTime("second",(seconds%3600)%60)})
+                                        formatTime("day",   (seconds / 86400) % 365),
+                                        formatTime("hour",  (seconds / 3600) % 24),
+                                        formatTime("minute",(seconds / 60) % 60),
+                                        formatTime("second",(seconds%3600) % 60)})
                         .filter(e -> !Objects.equals(e, ""))
                         .collect(Collectors.joining(", "))
                         .replaceAll(", (?!.+,)", " and ");
