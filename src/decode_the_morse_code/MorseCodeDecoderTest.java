9a097f71d7f6c18c2de4feb5112a484c3c194561
Kirill <seeraydary@gmail.com>
2023-07-04 15:33:44 +0300

    Расшифровка азбуки Морзе

---
+++ src/decode_the_morse_code/MorseCodeDecoderTest.java
@@ -0,0 +1,12 @@
+package decode_the_morse_code;
+
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
+public class MorseCodeDecoderTest {
+    @Test
+    public void testExampleFromDescription() {
+        assertThat(MorseCodeDecoder.decode(".... . -.--   .--- ..- -.. ."), is("HEY JUDE"));
+    }
+}
