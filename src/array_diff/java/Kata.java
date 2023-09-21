48b23a488b7799f30f133536f663e7503277d3a5
Kirill <seeraydary@gmail.com>
2023-09-21 17:12:11 +0300

    Принимает два массива чисел a, b. Возвращает массив, в котором есть числа в a, но нет в b

---
+++ src/array_diff/java/Kata.java
@@ -0,0 +1,28 @@
+package array_diff.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Kata {
+
+        public static int[] arrayDiff(int[] a, int[] b) {
+            List<Integer> list = new ArrayList<>();
+            for (int i : a) {
+                boolean isB = false;
+                for (int j : b) {
+                    if (i == j) {
+                        isB = true;
+                        break;
+                    }
+                }
+                if (!isB) {
+                    list.add(i);
+                }
+            }
+            int[] result = new int[list.size()];
+            for (int i = 0; i < list.size(); i++) {
+                result[i] = list.get(i);
+            }
+            return result;
+        }
+}
