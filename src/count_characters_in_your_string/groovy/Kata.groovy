731645e3ec77ed5454a696fb93d8c888b1f89bf4
Kirill <seeraydary@gmail.com>
2023-11-26 15:07:32 +0300

    Подсчитывает количество похожих символов в строке и заполняет мапу (символ: количество повторений)

---
+++ src/count_characters_in_your_string/groovy/Kata.groovy
@@ -0,0 +1,11 @@
+package count_characters_in_your_string.groovy
+
+class Kata {
+    static def count(string) {
+        def resultMap = [:]
+        string.each {
+            resultMap[it] = resultMap.getOrDefault(it, 0) + 1
+        }
+        resultMap
+    }
+}
