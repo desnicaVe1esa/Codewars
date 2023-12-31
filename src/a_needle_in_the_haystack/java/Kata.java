6c371dc1d8aa00d6cc73138484927ffe3fcfc2ab
Kirill <seeraydary@gmail.com>
2023-12-31 18:29:34 +0300

    Находит индекс слова needle в массиве

---
+++ src/a_needle_in_the_haystack/java/Kata.java
@@ -0,0 +1,10 @@
+package a_needle_in_the_haystack.java;
+
+import java.util.Arrays;
+
+public class Kata {
+    public static String findNeedle(Object[] haystack) {
+        int position = Arrays.stream(haystack).toList().indexOf("needle");
+        return "found the needle at position " + position;
+    }
+}
---
+++ src/a_needle_in_the_haystack/js/findNeedle.js
@@ -0,0 +1,5 @@
+function findNeedle(haystack) {
+    return 'found the needle at position ' + haystack.indexOf('needle');
+}
+
+module.exports = findNeedle;
