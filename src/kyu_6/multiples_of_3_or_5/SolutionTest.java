51b19fba7676f765995ac945e65c376682c3b6c2
Kirill <seeraydary@gmail.com>
2024-01-14 10:12:48 +0300

    Mass repo refactoring

--- src/kyu_6/multiples_of_3_or_5/SolutionTest.java
+++ src/kyu_6/multiples_of_3_or_5/SolutionTest.java
@@ -2,7 +2,6 @@ package kyu_6.multiples_of_3_or_5;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
-import multiples_of_3_or_5.*; // Заглушка от ошибки
 
 public class SolutionTest {
     @Test
