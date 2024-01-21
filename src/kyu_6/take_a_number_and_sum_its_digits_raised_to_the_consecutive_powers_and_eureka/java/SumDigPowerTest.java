a5b5deb0a4637b38f7c4f177ade76d63f2bc7123
Kirill <seeraydary@gmail.com>
2024-01-21 15:28:37 +0300

    Uncommented tests

--- src/kyu_6/take_a_number_and_sum_its_digits_raised_to_the_consecutive_powers_and_eureka/java/SumDigPowerTest.java
+++ src/kyu_6/take_a_number_and_sum_its_digits_raised_to_the_consecutive_powers_and_eureka/java/SumDigPowerTest.java
@@ -13,12 +13,12 @@ public class SumDigPowerTest {
     @Test
     public void test() {
         System.out.println("Basic Tests");
-//        testing(1, 10, new long[] {1, 2, 3, 4, 5, 6, 7, 8, 9});
+        testing(1, 10, new long[] {1, 2, 3, 4, 5, 6, 7, 8, 9});
         testing(1, 100, new long[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 89});
-//        testing(10, 100,  new long[] {89});
-//        testing(90, 100, new long[] {});
-//        testing(90, 150, new long[] {135});
-//        testing(50, 150, new long[] {89, 135});
-//        testing(10, 150, new long[] {89, 135});
+        testing(10, 100,  new long[] {89});
+        testing(90, 100, new long[] {});
+        testing(90, 150, new long[] {135});
+        testing(50, 150, new long[] {89, 135});
+        testing(10, 150, new long[] {89, 135});
     }
 }
