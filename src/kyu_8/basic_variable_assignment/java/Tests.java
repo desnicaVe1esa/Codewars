8513c90f1a4338828a7d4d34162e5f8764117486
Kirill <seeraydary@gmail.com>
2024-02-20 20:41:49 +0300

    init test

--- src/kyu_8/basic_variable_assignment/java/Tests.java
+++ src/kyu_8/basic_variable_assignment/java/Tests.java
@@ -2,7 +2,6 @@ package kyu_8.basic_variable_assignment.java;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
-import org.junit.runners.JUnit4;
 
 public class Tests {
     @Test
