9937a28bf3291b25a7df2b0fad6f252485b8ccb0
Kirill <seeraydary@gmail.com>
2024-05-24 10:20:39 +0300

    Возвращает все простые числа в заданном диапазоне

--- src/kyu_6/sieve_of_eratosthenes/java/Primes.java
+++ src/kyu_6/sieve_of_eratosthenes/java/Primes.java
@@ -1,24 +1,39 @@
 package kyu_6.sieve_of_eratosthenes.java;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
-import static java.math.BigInteger.valueOf;
-import static java.util.stream.IntStream.iterate;
 
 public class Primes {
     static public List<Integer> primes(Integer x){
-//        List<Integer> results = new ArrayList<Integer>();
-//        if (x <= 1) {
-//            return results;
-//        }
-//        results.add(2);
-//        for (int i = 2; i <= x; i++) {
-//            if (i % 2 != 0 && Math.sqrt(i) % 1 != 0) {
-//                results.add(i);
-//            }
-//        }
-//        return results;
+/*      1 вариант (прокатил :-))
+        List<Integer> results = new ArrayList<Integer>();
+        if (x <= 1) {
+            return results;
+        }
+        results.add(2);
+        for (int i = 2; i <= x; i++) {
+            if (i % 2 != 0 && Math.sqrt(i) % 1 != 0) {
+                results.add(i);
+            }
+        }
+        return results;
 
+        2 вариант
         return iterate(2, i -> i <= x, i -> i + 1).filter(i -> valueOf(i).isProbablePrime(10)).boxed().toList();
+
+        3 вариант                                                                                                     */
+        List<Integer> results = new ArrayList<Integer>();
+        boolean[] numbers = new boolean[x + 1];
+        Arrays.fill(numbers, true);
+        for(int i = 2; i < numbers.length; i++) {
+            if(numbers[i]) {
+                results.add(i);
+            }
+            for(int j = 2 * i; j < numbers.length; j += i) {
+                numbers[j] = false;
+            }
+        }
+        return results;
     }
 }
