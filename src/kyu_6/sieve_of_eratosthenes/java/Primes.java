658b5fd4ecdaa8f3f9f0c15f082321ca5a5ea9d3
Kirill <seeraydary@gmail.com>
2024-05-24 10:10:11 +0300

    Возвращает все простые числа в заданном диапазоне

--- src/kyu_6/sieve_of_eratosthenes/java/Primes.java
+++ src/kyu_6/sieve_of_eratosthenes/java/Primes.java
@@ -1,20 +1,24 @@
 package kyu_6.sieve_of_eratosthenes.java;
 
 import java.util.List;
-import java.util.ArrayList;
+
+import static java.math.BigInteger.valueOf;
+import static java.util.stream.IntStream.iterate;
 
 public class Primes {
     static public List<Integer> primes(Integer x){
-        List<Integer> results = new ArrayList<Integer>();
-        if (x <= 1) {
-            return results;
-        }
-        results.add(2);
-        for (int i = 2; i <= x; i++) {
-            if (i % 2 != 0 && Math.sqrt(i) % 1 != 0) {
-                results.add(i);
-            }
-        }
-        return results;
+//        List<Integer> results = new ArrayList<Integer>();
+//        if (x <= 1) {
+//            return results;
+//        }
+//        results.add(2);
+//        for (int i = 2; i <= x; i++) {
+//            if (i % 2 != 0 && Math.sqrt(i) % 1 != 0) {
+//                results.add(i);
+//            }
+//        }
+//        return results;
+
+        return iterate(2, i -> i <= x, i -> i + 1).filter(i -> valueOf(i).isProbablePrime(10)).boxed().toList();
     }
 }
