8502745a4048c62ba93ac07d33516bd137bfa9b6
Kirill <seeraydary@gmail.com>
2024-06-18 12:04:37 +0300

    Возвращает имя участника согласно условию

--- src/kyu_6/prize_draw/js/rankTest.js
+++ src/kyu_6/prize_draw/js/rankTest.js
@@ -1,9 +1,11 @@
 const rank = require("./rank");
-const assert = require('chai').assert3describe("Rank", function () {
-    it("Basic tests", function () {
+const assert = require('chai').assert
+
+
+describe("Rank",function() {
+    it("Basic tests",function() {
         assert.strictEqual(rank("Addison,Jayden,Sofia,Michael,Andrew,Lily,Benjamin", [4, 2, 1, 4, 3, 1, 2], 4), "Benjamin")
         assert.strictEqual(rank("Lagon,Lily", [1, 5], 2), "Lagon")
         assert.strictEqual(rank("Addison,Jayden,Sofia,Michael,Andrew,Lily,Benjamin", [4, 2, 1, 4, 3, 1, 2], 8), "Not enough participants")
         assert.strictEqual(rank("", [4, 2, 1, 4, 3, 1, 2], 6), "No participants")
-    })
-})
+    })})
