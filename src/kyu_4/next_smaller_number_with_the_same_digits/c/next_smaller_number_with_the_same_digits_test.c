94a683ce516fa1b43f8218660311ad60953526ca
Kirill <seeraydary@gmail.com>
2024-07-01 09:59:24 +0300

    Принимает положительное целое число и возвращает следующее меньшее положительное целое число, содержащее те же цифры

--- src/kyu_4/next_smaller_number_with_the_same_digits/c/next_smaller_number_with_the_same_digits_test.c
+++ src/kyu_4/next_smaller_number_with_the_same_digits/c/next_smaller_number_with_the_same_digits_test.c
@@ -1,4 +1,20 @@
-1
-#include <stdio.h>#include <criterion/criterion.h>4
-long long next_smaller_number(unsigned long long n);static void dotest(unsigned long long n, long long expected);7Test(the_multiply_function, should_pass_all_the_tests_provided) {    dotest(  21,  12);    dotest( 907, 790);    dotest( 531, 513);    dotest(1027,  -1);    dotest( 441, 414);    dotest(123456798, 123456789);}17
+#include <stdio.h>
+#include <criterion/criterion.h>
+
+long long next_smaller_number(unsigned long long n);
+static void dotest(unsigned long long n, long long expected);
+
+
+Test(the_multiply_function, should_pass_all_the_tests_provided) {
+    dotest(  21,  12);
+    dotest( 907, 790);
+    dotest( 531, 513);
+    dotest(1027,  -1);
+    dotest( 441, 414);
+    dotest(123456798, 123456789);
+}
+
 static void dotest(unsigned long long n, long long expected) {
+  long long actual = next_smaller_number(n);
+  cr_assert_eq(actual, expected, "Incorrect answer for n=%llu\n  Expected: %lld\n    Actual: %lld", n, expected, actual);
+}
