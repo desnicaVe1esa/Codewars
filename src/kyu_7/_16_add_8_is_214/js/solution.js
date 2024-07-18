675cd1fcc9abbd6de0458dd80c81ffdb33204dc7
Kirill <seeraydary@gmail.com>
2024-07-18 09:12:18 +0300

    Выводит сумму чисел игнорируя принцип переноса

--- src/kyu_7/_16_add_8_is_214/js/solution.js
+++ src/kyu_7/_16_add_8_is_214/js/solution.js
@@ -2,10 +2,10 @@ function add(num1, num2) {
     let result = "";
     while(num1 + num2) {
         result = num1 % 10 + num2 % 10 + result;
-        num1 = num1 / 10 | 0;
+        num1 = num1 / 10 | 0; // | 0 - приведение к целому типую. Если | 1 - приведение к целому типу + 1
         num2 = num2 / 10 | 0;
     }
-    return +result;
+    return +result; // +<something> - преобразование в число
 }
 
 module.exports = add;
