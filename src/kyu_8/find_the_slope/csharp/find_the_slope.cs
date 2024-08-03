2827a33f374ec09573df98a81ca3cf535d944abe
Kirill <seeraydary@gmail.com>
2024-08-03 17:13:52 +0300

    Возвращает строковое представление наклона линии (угловой коэффицент), соединяющей две точки.

--- src/kyu_8/find_the_slope/csharp/find_the_slope.cs
+++ src/kyu_8/find_the_slope/csharp/find_the_slope.cs
@@ -5,7 +5,7 @@ public class Slope
   public String slope(int[] points)
     {
           return points[2] - points[0] == 0 ? "undefined" :
-                  "" + points[3] - points[1]) / (points[2] - points[0]);
+                  "" + (points[3] - points[1]) / (points[2] - points[0]);
     }
 
 }
