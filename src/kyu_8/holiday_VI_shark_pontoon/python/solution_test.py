666c0dded407b41bf5311bf659796a24233f9dc4
Kirill <seeraydary@gmail.com>
2024-08-07 10:22:13 +0300

    Проверяет, спасется ли человек от акулы

---
+++ src/kyu_8/holiday_VI_shark_pontoon/python/solution_test.py
@@ -0,0 +1,8 @@
+def shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin):
+    if dolphin:
+        sharkSpeed = sharkSpeed * 0.5
+
+    if pontoonDistance / youSpeed < sharkDistance / sharkSpeed:
+        return "Alive!"
+    else:
+        return "Shark Bait!"
