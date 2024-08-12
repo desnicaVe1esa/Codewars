ee337f6f076fc143681f13f7b79ab2fd9f79d3b0
Kirill <seeraydary@gmail.com>
2024-08-12 16:30:27 +0300

    Вычисляет обратную польскую нотацию

---
+++ src/kyu_6/reverse_polish_notation_calculator/python/reverse_polish_notation_calculator_test.py
@@ -0,0 +1,7 @@
+test.assert_equals(calc(""), 0, "Should work with empty string")
+test.assert_equals(calc("3"), 3, "Should parse numbers")
+test.assert_equals(calc("3.5"), 3.5, "Should parse float numbers")
+test.assert_equals(calc("1 3 +"), 4, "Should support addition")
+test.assert_equals(calc("1 3 *"), 3, "Should support multiplication")
+test.assert_equals(calc("1 3 -"), -2, "Should support subtraction")
+test.assert_equals(calc("4 2 /"), 2, "Should support division")
