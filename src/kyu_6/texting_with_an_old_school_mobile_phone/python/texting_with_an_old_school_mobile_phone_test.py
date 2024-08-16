0b010311a38dce84967b0c9ad78167dd2f755486
Kirill <seeraydary@gmail.com>
2024-08-16 10:18:49 +0300

    Клавиатура кнопочного телефона

---
+++ src/kyu_6/texting_with_an_old_school_mobile_phone/python/texting_with_an_old_school_mobile_phone_test.py
@@ -0,0 +1,14 @@
+@test.describe('Sample tests')
+def sample_tests():
+  messages = [
+    ["hey", "4433999"],
+    ["one two three", "666 6633089666084477733 33"],
+    ["Hello World!", "#44#33555 5556660#9#66677755531111"],
+    ["Def Con 1!", "#3#33 3330#222#666 6601-1111"],
+    ["A-z", "#2**#9999"],
+    ["1984", "1-9-8-4-"],
+    ["Big thanks for checking out my kata", "#22#444 4084426655777703336667770222443322255444664066688 806999055282"]
+  ]
+
+  for message, answer in messages:
+    test.assert_equals(send_message(message), answer, 'Message: ' + message)
