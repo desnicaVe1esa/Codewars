7aa005bdea4eb7a23e8f20315a7ff5969c44d218
Kirill <seeraydary@gmail.com>
2024-08-25 10:29:14 +0300

    Суммирует два наименьших элемента массива

---
+++ src/kyu_7/sum_of_two_lowest_positive_integers/csharp/sum_of_two_lowest_positive_integers.cs
@@ -0,0 +1,10 @@
+using System;
+
+public static class Kata
+{
+	public static int sumTwoSmallestNumbers(int[] numbers)
+	{
+        Array.Sort(numbers);
+        return numbers[0] + numbers[1];
+	}
+}
