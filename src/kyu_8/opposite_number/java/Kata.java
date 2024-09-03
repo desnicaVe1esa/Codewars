1c9871fc3a1dd629ad1f19377d7e7913c42b81f1
Kirill <seeraydary@gmail.com>
2024-09-03 07:56:52 +0300

    Возвращает входящее число с противоположным знаком

---
+++ src/kyu_8/opposite_number/java/Kata.java
@@ -0,0 +1,8 @@
+package kyu_8.opposite_number.java;
+
+
+public class Kata {
+    public static int opposite(int number) {
+        return number * -1;
+    }
+}
---
+++ src/kyu_8/opposite_number/java/OppositeExampleTests.java
@@ -0,0 +1,11 @@
+package kyu_8.opposite_number.java;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class OppositeExampleTests {
+    @Test
+    public void tests() {
+        assertEquals(-1, Kata.opposite(1));
+    }
+}
