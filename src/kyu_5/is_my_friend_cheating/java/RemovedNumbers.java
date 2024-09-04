2b5a34b509ab5dda35bae38374877be85c599658
Kirill <seeraydary@gmail.com>
2024-09-04 13:17:08 +0300

    Возвращает числа, исключенные из последовательности

---
+++ src/kyu_5/is_my_friend_cheating/java/RemovedNumbers.java
@@ -0,0 +1,20 @@
+package kyu_5.is_my_friend_cheating.java;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class RemovedNumbers {
+
+    public static List<long[]> removNb(long n) {
+        List<long[]> result = new ArrayList<long[]>();
+        long sum = n * (n + 1) / 2;
+        for (long i = 1; i <= n; i++) {
+            long j = (sum - i) / (i + 1);
+            if (j <= n && i * j == sum - i - j) {
+                result.add(new long[] {i, j});
+            }
+        }
+        return result;
+    }
+
+}
---
+++ src/kyu_5/is_my_friend_cheating/java/RemovedNumbersTest.java
@@ -0,0 +1,28 @@
+package kyu_5.is_my_friend_cheating.java;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertTrue;
+
+public class RemovedNumbersTest {
+    @Test
+    public void test12() {
+        List<long[]> res = new ArrayList<long[]>();
+        res.add(new long[] {15, 21});
+        res.add(new long[] {21, 15});
+        List<long[]> a = RemovedNumbers.removNb(26);
+        assertArrayEquals(res.get(0), a.get(0));
+        assertArrayEquals(res.get(1), a.get(1));
+    }
+    @Test
+    public void test14() {
+        List<long[]> res = new ArrayList<long[]>();
+        List<long[]> a = RemovedNumbers.removNb(100);
+        assertTrue(res.size() == a.size());
+    }
+}
+
