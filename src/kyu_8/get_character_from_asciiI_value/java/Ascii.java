1331d9e08ff9f924ba217e3202cf3419774633f0
Kirill <seeraydary@gmail.com>
2024-09-16 17:50:44 +0300

    Возвращает символ по коду

---
+++ src/kyu_8/get_character_from_asciiI_value/java/Ascii.java
@@ -0,0 +1,7 @@
+package kyu_8.get_character_from_asciiI_value.java;
+
+public class Ascii {
+    public static char getChar(int c) {
+        return (char) c;
+    }
+}
---
+++ src/kyu_8/get_character_from_asciiI_value/java/SolutionTest.java
@@ -0,0 +1,24 @@
+package kyu_8.get_character_from_asciiI_value.java;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void testChar() {
+        assertEquals('7', Ascii.getChar(55));
+        assertEquals('8', Ascii.getChar(56));
+        assertEquals('9', Ascii.getChar(57));
+        assertEquals(':', Ascii.getChar(58));
+        assertEquals(';', Ascii.getChar(59));
+        assertEquals('<', Ascii.getChar(60));
+        assertEquals('=', Ascii.getChar(61));
+        assertEquals('>', Ascii.getChar(62));
+        assertEquals('?', Ascii.getChar(63));
+        assertEquals('@', Ascii.getChar(64));
+        assertEquals('A', Ascii.getChar(65));
+        assertEquals("getChar should return a `char`", 0, Character.compare('!', Ascii.getChar(33)));
+    }
+}
+
