68243da734aad43d58acbe204409c4b5c71e327b
Kirill <seeraydary@gmail.com>
2024-09-16 17:24:21 +0300

    Hello World!

---
+++ src/kyu_8/classic_hello_world/java/Solution.java
@@ -0,0 +1,7 @@
+package kyu_8.classic_hello_world.java;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/kyu_8/classic_hello_world/java/SolutionTest.java
@@ -0,0 +1,11 @@
+package kyu_8.classic_hello_world.java;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+    @Test
+    void testSomething() {
+        // assertEquals("expected", "actual");
+    }
+}
+
