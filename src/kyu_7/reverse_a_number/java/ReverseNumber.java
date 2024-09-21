ef5b398ed41d2bf1685eeebacf35c44cb83804f7
Kirill <seeraydary@gmail.com>
2024-09-21 19:39:32 +0300

    Разворачивает число

--- src/kyu_7/reverse_a_number/java/ReverseNumber.java
+++ src/kyu_7/reverse_a_number/java/ReverseNumber.java
@@ -5,7 +5,7 @@ public class ReverseNumber {
     public static int reverse(int number) {
         int modNum = Math.abs(number);
         StringBuilder prepared = new StringBuilder();
-        int result = 0;
+
         while (modNum != 0) {
             prepared.append(modNum % 10);
             modNum /= 10;
