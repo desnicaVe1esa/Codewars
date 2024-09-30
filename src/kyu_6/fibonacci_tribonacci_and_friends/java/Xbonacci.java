397c8294d5187a1f808853a0df72c3c01c2b085a
Kirill <seeraydary@gmail.com>
2024-09-30 11:05:40 +0300

    Функция Xbonacci, которая принимает сигнатуру из X элементов

---
+++ src/kyu_6/fibonacci_tribonacci_and_friends/java/Xbonacci.java
@@ -0,0 +1,18 @@
+package kyu_6.fibonacci_tribonacci_and_friends.java;
+
+public class Xbonacci {
+
+    public double[] xbonacci(double[] signature, int n) {
+        double[] result = new double[n];
+        for (int i = 0; i < n; i++) {
+            if (i < signature.length) {
+                result[i] = signature[i];
+            } else {
+                for (int j = 1; j <= signature.length; j++) {
+                    result[i] += result[i - j];
+                }
+            }
+        }
+        return result;
+    }
+}
