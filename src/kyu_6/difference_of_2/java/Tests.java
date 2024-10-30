e3da28cc4800a4be16a824ea767ed689b4839c37
Kirill <seeraydary@gmail.com>
2024-10-30 09:59:39 +0300

    Uncommented the tests.

--- src/kyu_6/difference_of_2/java/Tests.java
+++ src/kyu_6/difference_of_2/java/Tests.java
@@ -7,15 +7,15 @@ import static org.junit.Assert.assertArrayEquals;
 public class Tests {
     @Test
     public void sample_tests() {
-//        assertArrayEquals(
-//                new int[][]{{1, 3}, {2, 4}},
-//                Kata.twosDifference(new int[]{1, 2, 3, 4})
-//        );
-//
-//        assertArrayEquals(
-//                new int[][]{{1, 3}, {4, 6}},
-//                Kata.twosDifference(new int[]{1, 3, 4, 6})
-//        );
+        assertArrayEquals(
+                new int[][]{{1, 3}, {2, 4}},
+                Kata.twosDifference(new int[]{1, 2, 3, 4})
+        );
+
+        assertArrayEquals(
+                new int[][]{{1, 3}, {4, 6}},
+                Kata.twosDifference(new int[]{1, 3, 4, 6})
+        );
 
         assertArrayEquals(
                 new int[][]{{1, 3}, {3, 5}, {4, 6}},
