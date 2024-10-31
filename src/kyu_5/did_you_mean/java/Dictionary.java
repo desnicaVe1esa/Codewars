bd02232f9d3d4aa06b37dd13c0c4010cf364c054
Kirill <seeraydary@gmail.com>
2024-10-31 13:17:53 +0300

    Аналог Google's did you mean?

--- src/kyu_5/did_you_mean/java/Dictionary.java
+++ src/kyu_5/did_you_mean/java/Dictionary.java
@@ -1,5 +1,8 @@
 package kyu_5.did_you_mean.java;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class Dictionary {
 
     private final String[] words;
@@ -9,7 +12,23 @@ public class Dictionary {
     }
 
     public String findMostSimilar(String to) {
+        return Arrays.stream(words)
+                .min((a, b) -> getSimilarPoint(a, to) - getSimilarPoint(b, to))
+                .get();
+    }
 
-        return "";
+    private int getSimilarPoint(String a, String b) {
+        return Math.max(a.length(), b.length()) - ((int) sameLettersLength(a, b));
+    }
+
+    private long sameLettersLength(String a, String b) {
+        if (a.length() > b.length()) {
+            return sameLettersLength(b, a);
+        }
+
+        return IntStream.rangeClosed(0, b.length() - a.length())
+                .mapToLong(i -> IntStream.range(0, a.length()).filter(j -> a.charAt(j) == b.charAt(i + j)).count())
+                .max()
+                .orElse(0);
     }
 }
