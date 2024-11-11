22b759ae018797bcb621622dcb4c9dadbb6e26e2
Kirill <seeraydary@gmail.com>
2024-11-11 07:48:39 +0300

    Находит недостающий член в арифмитической прогрессии.

--- src/kyu_6/find_the_missing_term_in_an_arithmetic_progression/java/Solution.java
+++ src/kyu_6/find_the_missing_term_in_an_arithmetic_progression/java/Solution.java
@@ -2,7 +2,11 @@ package kyu_6.find_the_missing_term_in_an_arithmetic_progression.java;
 
 public class Solution {
     public static int findMissing(int[] numbers) {
-        int firstEl, lastEl, s, i, sum = 0, missingnumber;
+        int firstEl;
+        int lastEl;
+        int s;
+        int sum = 0;
+        int missingNumber;
         firstEl = numbers[0];
         lastEl = numbers[numbers.length - 1];
         if ((firstEl + lastEl) % 2 == 0) {
@@ -12,12 +16,12 @@ public class Solution {
             s = (numbers.length + 1) / 2;
             s = (firstEl + lastEl) * s;
         }
-        for (i = 0; i <= numbers.length - 1; i++) {
+        for (int i = 0; i <= numbers.length - 1; i++) {
             sum = sum + numbers[i];
         }
-        missingnumber = s - sum;
+        missingNumber = s - sum;
 
-        return missingnumber;
+        return missingNumber;
     }
 }
 
