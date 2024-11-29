8c8febf91a11cdd7e446f4b025f0482030dbfe54
Kirill <seeraydary@gmail.com>
2024-11-29 16:45:27 +0300

    Возвращает первое число треугольника, содержащее более "n" делителей (n > 0).

--- src/kyu_6/factors_of_triangle_numbers/java/TriangleFactorsTest.java
+++ src/kyu_6/factors_of_triangle_numbers/java/TriangleFactorsTest.java
@@ -9,8 +9,8 @@ public class TriangleFactorsTest {
     @Test
     public void test1() {
         TriangleFactors tf = new TriangleFactors();
-//        assertEquals(3, tf.calculate(1));
-//        assertEquals(6, tf.calculate(2));
+        assertEquals(3, tf.calculate(1));
+        assertEquals(6, tf.calculate(2));
         assertEquals(28, tf.calculate(5));
     }
 }
