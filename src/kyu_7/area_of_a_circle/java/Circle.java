ffbd612127a39c601ce4d492f0c33471bc5d9486
Kirill <seeraydary@gmail.com>
2024-12-02 16:29:21 +0300

    Находит площадь круга.

--- src/kyu_7/area_of_a_circle/java/Circle.java
+++ src/kyu_7/area_of_a_circle/java/Circle.java
@@ -3,7 +3,7 @@ package kyu_7.area_of_a_circle.java;
 public class Circle {
 
     public static double area(double radius) {
-        if ( radius < 1) {
+        if (radius < 1) {
             throw new IllegalArgumentException();
         }
         return Math.PI * Math.pow(radius, 2);
