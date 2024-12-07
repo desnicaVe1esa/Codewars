c237bca55f67a703182daffc40f328cc2ba82955
debugger4O4 <now-kirill@ya.ru>
2024-12-07 11:34:54 +0300

    Находит минимальное количество прыжков от начала до конца.

---
+++ src/kyu_7/cats_and_shelves/java/Kata.java
@@ -0,0 +1,7 @@
+package kyu_7.cats_and_shelves.java;
+
+public class Kata {
+    public static int solution(int start, int finish) {
+        return (finish - start) / 3 + (finish - start) % 3;
+    }
+}
---
+++ src/kyu_7/cats_and_shelves/java/SolutionTest.java
@@ -0,0 +1,13 @@
+package kyu_7.cats_and_shelves.java;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+    @Test
+    @DisplayName("Test case in description")
+    void sampleTest() {
+        assertEquals(2, Kata.solution(1, 5));
+    }
+}
